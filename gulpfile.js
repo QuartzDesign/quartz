5b6cd61f482938375a51e18e9e980b551f000403
Corbin Matschull <qween@tutanota.com>
2016-09-03 22:09:03 -0600

    Inital Commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var gulp       = require('gulp');
+var sass       = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var webserver  = require('gulp-webserver');
+
+gulp.task('sass-expanded', function() {
+    return gulp.src('sass/quartz.sass')
+               .pipe(sourcemaps.init())
+               .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+               .pipe(sass().on('error', sass.logError))
+               .pipe(sourcemaps.write('maps/'))
+               .pipe(gulp.dest('css/quartz-expanded'))
+})
+
+gulp.task('sass-compressed', function() {
+    return gulp.src('sass/quartz.sass')
+               .pipe(sourcemaps.init())
+               .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+               .pipe(sass().on('error', sass.logError))
+               .pipe(sourcemaps.write('maps/'))
+               .pipe(gulp.dest('css/quartz-minified'))
+})
+
+gulp.task('sass:watch', function() {
+    gulp.watch('sass/**/*.sass', ['sass-compressed', 'sass-expanded'])
+})
+
+gulp.task('default', ['sass-expanded', 'sass-compressed'])
---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() >= 300) {
+            $('.navbar, .expo-blur-background').addClass('nse-scrolled--from--top');
+        }else {
+            $('.navbar, .expo-blur-background').removeClass('nse-scrolled--from--top');
+        }
+    })
+
+    $('[scroll-target]').click(function(){
+        var $targetObj = $('#' + $(this).attr('scroll-target')), $this = $(this)
+        $("html, body").animate({ scrollTop: $targetObj.offset().top }, 800, 'easeInOutQuart')
+    })
+
+    $('.en-l').hover(function(e) {
+        $(e.target).closest('li').toggleClass('sensed')
+    })
+})
